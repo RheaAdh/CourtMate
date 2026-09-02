2cd394204c36bd112d1b3fc7af4adf15d097e543
Rhea Adhikari <rheadhikari@gmail.com>
2026-09-02 05:55:18 +0530

    login fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/:path*',
+        destination: 'https://firebaseapp.com*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
